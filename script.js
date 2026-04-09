11b7fdfde8650bbf51de91b4081a94eb9b22d250
Yurizcdev <yurizcrestani@gmail.com>
2026-04-09 20:42:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function entrar() {
     let usuario = document.getElementById("usuario").value;
     let senha = document.getElementById("senha").value;
 
-    if (usuario == "admin" && senha == "123") {
+    if (usuario == "adm" && senha == "123") {
         document.getElementById("mensagem").innerText = "Login correto";
     } else {
         document.getElementById("mensagem").innerText = "Usuário ou senha incorretos";
